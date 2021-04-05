b01cf4e57e88fee76c7ddc18e27950f52a13a5ad
Johnny Backlund <johnnybacklund@hotmail.com>
2021-04-05 22:04:19 +0200

    Got basic test going

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import './App.scss';
+
+const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      
+      It's <time dateTime={response}>{response}</time>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIo = require('socket.io')
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());  // enable pre-flight
+
 
 const port = process.env.PORT || 4001
 const index = require('./routes/index');
-
-const app = express();
 app.use(index);
 
 const server = http.createServer(app);
-const io = socketIo(server);
-const getApiAndEmit = "TODO";
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+let interval;
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        clearInterval(interval);
+    });
+});
+
+const getApiAndEmit = socket => {
+    const response = new Date()
+
+    socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
